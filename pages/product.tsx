4421b4cd7c21fc72e81f3fa992f55ff0ece23bdd
GORDIAN12 <davidgordian667@gmail.com>
2026-03-10 13:53:36 -0600

    create funtion product

--- pages/product.tsx
+++ pages/product.tsx
@@ -130,7 +130,7 @@ function ConsultationForm() {
         </div>
     );
 }
-
+//function Product
 export default function Product() {
     return (
         <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
